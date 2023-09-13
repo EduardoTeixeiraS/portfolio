284e566bb24a894c9d1d8bcda42b382059105998
Eduardo Teixeira <dudu.teixeira197@gmail.com>
2023-09-13 15:33:52 -0300

    Criando o esqueleto do site

---
+++ src/containers/Sidebar/index.tsx
@@ -0,0 +1,10 @@
+import Titulo from '../../components/Titulo'
+
+const Sidebar = () => (
+  <aside>
+    <img src="https://github.com/eduardoteixeiras.png" />
+    <Titulo>Eduardo Teixeira</Titulo>
+  </aside>
+)
+
+export default Sidebar
---
+++ src/containers/Sobre/index.tsx
@@ -0,0 +1,15 @@
+import Titulo from '../../components/Titulo'
+
+const Sobre = () => (
+  <section>
+    <Titulo>Sobre mim</Titulo>
+    <p>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et totam dolorum
+      sapiente provident quod quasi natus itaque, aut quam perferendis commodi
+      perspiciatis. Repellendus numquam sint, molestiae nisi praesentium
+      similique qui?
+    </p>
+  </section>
+)
+
+export default Sobre
